6b91ab19c251d10fc979bf5da4cba4c27844d859
artoymsd <artoymsd@dline-media.com>
2019-01-29 13:05:03 +0300

    Initial Commit

---
+++ VPNService/VPNService.h
@@ -0,0 +1,19 @@
+//
+//  VPNService.h
+//  VPNService
+//
+//  Created by Artem Sidorenko on 29/01/2019.
+//  Copyright © 2019 Artem Sidorenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VPNService.
+FOUNDATION_EXPORT double VPNServiceVersionNumber;
+
+//! Project version string for VPNService.
+FOUNDATION_EXPORT const unsigned char VPNServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VPNService/PublicHeader.h>
+
+
